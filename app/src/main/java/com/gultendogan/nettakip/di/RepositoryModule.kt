f253e9eb56fc7627808048f22bbef654b7650850
gulten27 <ggdogann00@gmail.com>
2022-10-05 19:44:56 +0300

    commit

---
+++ app/src/main/java/com/gultendogan/nettakip/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.gultendogan.nettakip.di
+
+import com.gultendogan.nettakip.data.local.NetDao
+import com.gultendogan.nettakip.data.repository.NetRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class RepositoryModule {
+    @Provides
+    fun provideNetRepository(
+        dbDao: NetDao
+    ): NetRepository {
+        return NetRepository(dbDao)
+    }
+}
---
+++ app/src/main/java/com/gultendogan/nettakip/di/RoomModule.kt
@@ -0,0 +1,31 @@
+package com.gultendogan.nettakip.di
+
+import android.content.Context
+import androidx.room.Room
+import com.gultendogan.nettakip.data.local.AppDatabase
+import com.gultendogan.nettakip.data.local.NetDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+const val DATABASE_NAME="Net_ly"
+
+@[Module InstallIn(SingletonComponent::class)]
+object RoomModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(
+        db: AppDatabase
+    ): NetDao = db.netDao()
+}
